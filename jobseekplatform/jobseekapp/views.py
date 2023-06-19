82bcebc6845159c9b7ae082f439e3042aa1afd90
chandant9 <discoverchan9@gmail.com>
2023-06-19 14:18:11 -0300

    defined API views and urls for endpoints

--- jobseekplatform/jobseekapp/views.py
+++ jobseekplatform/jobseekapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@csrf_exempt
+def job_list(request):
+    if request.method == 'GET':
+        jobs = Job.objects.all()
+        job_data = [{'title': job.title, 'description': job.description} for job in jobs]
+        return JsonResponse({'jobs': job_data})
+
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        job = Job.objects.create(title=title, description=description)
+        return JsonResponse({'message': 'Job created successfully'})
