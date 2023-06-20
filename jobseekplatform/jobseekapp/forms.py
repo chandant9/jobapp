9803b532d143134a8ba39cb7b51a03d46af13359
chandant9 <discoverchan9@gmail.com>
2023-06-20 16:57:19 -0300

    implemented models and forms for resume upload

--- jobseekplatform/jobseekapp/forms.py
+++ jobseekplatform/jobseekapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Job, Resume
 
 
 class ProfileForm(forms.ModelForm):
@@ -20,3 +21,20 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class JobSearchForm(forms.Form):
+    keyword = forms.CharField(max_length=100, required=False)
+    location = forms.CharField(max_length=100, required=False)
+
+
+class ResumeUploadForm(forms.ModelForm):
+    class Meta:
+        model = Resume
+        fields = ['file']
+
+
+class JobPostForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ['title', 'description', 'location', 'salary']
--- jobseekplatform/jobseekapp/models.py
+++ jobseekplatform/jobseekapp/models.py
@@ -26,9 +26,19 @@ class Job(models.Model):
 
 
 class Application(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applications')
     applicant = models.ForeignKey(User, on_delete=models.CASCADE)
     applied_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Application for {self.job.title} by {self.applicant.username}"
+
+
+class Resume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='resumes/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Resume for {self.user.username}"
+
